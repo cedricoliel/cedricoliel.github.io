5672e04a3218f5728646cc398d56bcf208097d09
Gabin221 <serrurot19@gmail.com>
2024-06-20 09:44:56 +0200

    Update

---
+++ Scripts/brzgobrgrpjebnvaffzea.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA45vEeQW1bb2YR557QuMgy_4BLDVsWhN8",
+    authDomain: "cedricoliel-b80b3.firebaseapp.com",
+    projectId: "cedricoliel-b80b3",
+    storageBucket: "cedricoliel-b80b3.appspot.com",
+    messagingSenderId: "352068008407",
+    appId: "1:352068008407:web:52afbcb4e42eb308e07392"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
