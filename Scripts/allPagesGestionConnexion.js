43bea80725d5341ac981ed162d9c52adf1f4b9d4
Gabin221 <serrurot19@gmail.com>
2024-06-24 23:28:57 +0200

    Update allPagesGestionConnexion.js

--- Scripts/allPagesGestionConnexion.js
+++ Scripts/allPagesGestionConnexion.js
@@ -1,42 +1,23 @@
-function encryptPassword(password) {
-    return CryptoJS.SHA256(password).toString();
-}
-
-// Check if the user has a valid code in local storage
 function checkLocalCode() {
     const storedCode = localStorage.getItem('Code');
-    var validInput = true;
     if (storedCode) {
-        var code = db.collection('Code');
-        code.where('Valeur', '==', storedCode).get()
-            .then((querySnapshot) => {
-                if (querySnapshot.empty) {
-                    validInput = false;
-                }
-            })
-            .catch((error) => {
-                console.error('Erreur lors de la vérification du code:', error);
-                validInput = false;
-            });
-
         var codeBis = db.collection('SU');
         codeBis.where('Valeur', '==', storedCode).get()
             .then((querySnapshot) => {
-                if (!querySnapshot.empty) {
-                    document.getElementById("diapos").style.display = "block";
-                    document.getElementById("musiques").style.display = "block";
+                if (querySnapshot.empty) {
+                    window.location.href = '../index.html';
                 }
             })
             .catch((error) => {
                 console.error('Erreur lors de la vérification du code:', error);
-                validInput = false;
+                window.location.href = '../index.html';
             });
-    }
-
-    if (!validInput) {
+    } else {
         window.location.href = '../index.html';
     }
 }
 
 // Run the check on page load
-window.onload = checkLocalCode;
+window.onload = function() {
+    checkLocalCode();
+};
